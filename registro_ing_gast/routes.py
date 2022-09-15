b03920a5259bffce60eca97f172c48038c2c353e
Alvaritto951 <alvaritto951@gmail.com>
2022-09-15 23:30:41 +0200

    Plantillas con base & extends

--- registro_ing_gast/routes.py
+++ registro_ing_gast/routes.py
@@ -1,6 +1,12 @@
+from flask import render_template
 from registro_ing_gast import app
 
 
 @app.route("/index")
 def index():
-    return "Servidor funcionando"
+    return render_template("index.html", pageTitle="Lista", movimientos=[])
+
+
+@app.route("/nuevo")
+def nuevo():
+    return render_template("new.html", pageTitle="Alta")
